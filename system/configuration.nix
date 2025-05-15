371ef8057c027720c81822dca0e0b3f13f9b306d
Amaury Pupo Merino <amaury.pupo@gmail.com>
2025-05-15 19:24:28 -0400

    Reorganize configuration

--- system/configuration.nix
+++ system/configuration.nix
@@ -140,18 +140,19 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    helix
-    git
+    atool
+    bat
     eza
     fish
-    bat
-    starship
+    git
+    helix
     neofetch
-    atool
     python314
     R
+    rPackages.tidyverse
+    starship
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
   ];
 
 }
